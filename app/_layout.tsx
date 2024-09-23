a5d5e722ab5e7331b761f3f70ed92eaf6d17a24b
Mates24 <matejcikmathias@gmail.com>
2024-09-23 12:05:29 +0200

    Added Sign Up to Tab screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { Stack, Tabs } from "expo-router";
+import { Tabs } from "expo-router";
 import { TabBarIcon } from "@/components/navigation/TabBarIcon";
 
 export default function RootLayout() {
@@ -9,6 +9,15 @@ export default function RootLayout() {
         name="login"
         options={{
           title: 'Login',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen 
+        name="signup"
+        options={{
+          title: 'Sign Up',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
           ),
