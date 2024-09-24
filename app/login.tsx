31d8d0963856493fd24faad04362de95ba4beab5
Mates24 <matejcikmathias@gmail.com>
2024-09-24 13:34:17 +0200

    Modified styling and added navigation to Sign Up

--- app/login.tsx
+++ app/login.tsx
@@ -1,41 +1,47 @@
 import { Button, SafeAreaView, TextInput, Text, View, TouchableOpacity } from "react-native";
+import { NavigationProp } from '@react-navigation/native';
 
-const Login = () => {
+interface Props{
+  navigation: NavigationProp<any>;
+}
+
+const Login = ({ navigation }: Props) => {
   return (
     <SafeAreaView
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        gap: 20,
+        gap: 15,
       }}
     >
-        <Text style={{fontSize: 32}}>Prihláste sa</Text>
-        <View style={{width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-          <Text>E-mail:</Text>
+        <Text style={{fontSize: 32, textTransform: 'uppercase', fontWeight: 700}}>Prihláste sa</Text>
+        <View style={{width: '80%', display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 10}}>
+          <Text style={{alignSelf: 'flex-start', paddingLeft: 10}}>E-mail:</Text>
           <TextInput
               placeholder="E-mail"
               autoCapitalize="none"
               keyboardType="email-address"
               inputMode="email"
-              style={{paddingLeft: 20, width: '90%', height: 40, color: '#fff', borderRadius: 10, backgroundColor: 'black'}}
+              style={{paddingLeft: 10, width: '100%', height: 40, color: '#fff', borderRadius: 10, backgroundColor: 'black'}}
           />
         </View>
-        <View style={{width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-          <Text>Password:</Text>
+        <View style={{width: '80%', display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 10}}>
+          <Text style={{alignSelf: 'flex-start', paddingLeft: 10}}>Password:</Text>
           <TextInput
               placeholder="Password"
               secureTextEntry={true}
-              style={{paddingLeft: 20, width: '90%', height: 40, color: '#fff', borderRadius: 10, backgroundColor: 'black'}}
+              style={{paddingLeft: 10, width: '100%', height: 40, color: '#fff', borderRadius: 10, backgroundColor: 'black'}}
           />
         </View>
         <Button 
             title="Prihlásiť sa"
+            color={'#006cff'}
             onPress={() => console.log('Prihlásený')}
         />
         <View style={{width: '100%', display: 'flex', alignItems: 'center'}}>
           <Text>Ešte nemáte účet?</Text>
-          <TouchableOpacity onPress={() => console.log('Registrovať sa')}>
+          <TouchableOpacity onPress={() => navigation.navigate('Signup')}>
             <Text style={{color: 'red', paddingBottom: 0,}}>Registrovať sa</Text>
           </TouchableOpacity>
         </View>
