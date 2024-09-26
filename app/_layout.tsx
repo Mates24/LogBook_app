a7932432c5f2169b97028b8d240171a8883a2a5f
Mates24 <matejcikmathias@gmail.com>
2024-09-26 13:50:17 +0200

    Turned off headers for login and singup page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./login";
-import Signup from "./signup";
+import SignUp from "./signup";
 
 const Stack = createNativeStackNavigator();
 
@@ -10,8 +10,8 @@ export default function RootLayout() {
   return (
     <NavigationContainer independent={true}>
       <Stack.Navigator initialRouteName='Login'>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
+        <Stack.Screen name="Signup" component={SignUp} options={{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
