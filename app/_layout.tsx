de95bc027a6bc87eb7cf9b692db4008c9c5bf2e1
Mates24 <matejcikmathias@gmail.com>
2024-10-12 16:38:22 +0200

    Added home screen to Navigation container

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./login";
 import SignUp from "./signup";
+import Home from "./home";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ export default function RootLayout() {
       <Stack.Navigator initialRouteName='Login'>
         <Stack.Screen name="Login" component={Login} options={{headerShown: false}}/>
         <Stack.Screen name="Signup" component={SignUp} options={{headerShown: false}}/>
+        <Stack.Screen name="Home" component={Home} options={{headerShown: false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
