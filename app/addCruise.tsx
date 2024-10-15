f7b15bcce908b86e8785e7952bb6bd755555f4f9
Mates24 <matejcikmathias@gmail.com>
2024-10-15 15:52:01 +0200

    Created structure of screen

--- app/addCruise.tsx
+++ app/addCruise.tsx
@@ -1,8 +1,50 @@
 import * as React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { SafeAreaView, View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
 
 const Cruise = () => {
     <SafeAreaView>
-        
+        <View>
+            <TouchableOpacity>
+                <Image />
+                <Text>Späť</Text>
+            </TouchableOpacity>
+        </View>
+        <View>
+            <Text>Plavba</Text>
+            <TextInput />
+            <TextInput />
+            <TextInput />
+        </View>
+        <View>
+            <Text>Plavidlo</Text>
+            <TextInput />
+            <TextInput />
+            <TextInput />
+            <TextInput />
+            <TextInput />
+            <TextInput />
+        </View>
+        <View>
+            <Text>Skipper</Text>
+            <TextInput />
+            <TextInput />
+        </View>
+        <View>
+            <Text>Posádka</Text>
+            <TextInput />
+            <TextInput />
+            <TextInput />
+            <TouchableOpacity>
+                <Text>Pridať člena</Text>
+            </TouchableOpacity>
+        </View>
+        <View>
+            <TouchableOpacity>
+                <Text>Zrušiť</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+                <Text>Pridať</Text>
+            </TouchableOpacity>
+        </View>
     </SafeAreaView>
 }
