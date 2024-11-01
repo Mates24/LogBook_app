3f5c744c68e9785ea3e92f0222dbd63b40bb73f9
Mates24 <matejcikmathias@gmail.com>
2024-11-01 18:26:03 +0100

    Added loading and refresh after new cruise is added

--- app/home.tsx
+++ app/home.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, lazy} from 'react';
-import { NavigationProp } from '@react-navigation/native';
-import { SafeAreaView, ScrollView, View, TouchableOpacity, Image, Text, Dimensions } from 'react-native';
+import React, { useState, useEffect, useCallback } from 'react';
+import { NavigationProp, useFocusEffect } from '@react-navigation/native';
+import { SafeAreaView, ScrollView, View, TouchableOpacity, Image, Text, Dimensions, ActivityIndicator } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { BarChart } from 'react-native-chart-kit';
 import Pocketbase from 'pocketbase';
@@ -21,7 +21,8 @@ interface Cruise {
     time: number;
 };
 
-const Home = ({ navigation }: Props) => {    
+const Home = ({ navigation }: Props) => {
+    const [loading, setLoading] = useState(true);
     const [cruises, setCruises] = useState<Cruise[]>([]);
     const [userCruisesIds, setUserCruisesIds] = useState<string[]>([]);
     const [weatherData, setWeatherData] = useState<any>(null);
@@ -29,15 +30,15 @@ const Home = ({ navigation }: Props) => {
     const [locationError, setLocationError] = useState<string | null>(null);
     const [url, setUrl] = useState<string>('');
 
-
+    
     // Get users avatar
     const getUserAvatar = async () => {
+        const pb = new Pocketbase('https://mathiasdb.em1t.xyz/');
         const user = await AsyncStorage.getItem('user');
         if(user){
             const userData = JSON.parse(user);
             const userEmail = userData.email;
             const userPassword = userData.password;
-            const pb = new Pocketbase('https://mathiasdb.em1t.xyz/');
             await pb.collection('users').authWithPassword(userEmail, userPassword);
             const userAvatar = pb.files.getUrl(userData.record, userData.record.avatar, {'thumb': '100x250'});
             setUrl(userAvatar);
@@ -91,6 +92,7 @@ const Home = ({ navigation }: Props) => {
 
     // Cruises
     const getCruise = async () => {
+        setLoading(true);
         const pb = new Pocketbase('https://mathiasdb.em1t.xyz/');
         const user = await AsyncStorage.getItem('user');
 
@@ -102,10 +104,13 @@ const Home = ({ navigation }: Props) => {
                 const userData = JSON.parse(user);
                 const userEmail = userData.email;
                 const userPassword = userData.password;
-                const cruisesIds = userData.record.cruises;
-
+                
                 await pb.collection('users').authWithPassword(userEmail, userPassword);
 
+                const record = await pb.collection('users').getOne(userData.record.id);
+
+                const cruisesIds = record.cruises;
+                
                 if (cruisesIds && cruisesIds.length > 0) {
                     setUserCruisesIds(cruisesIds);
 
@@ -120,9 +125,9 @@ const Home = ({ navigation }: Props) => {
                         id: record.id,
                         country: record.country,
                         date: record.from.split(' ')[0].split('-').reverse().join('.') + ' - ' + record.to.split(' ')[0].split('-').reverse().join('.'),
-                        sails: record.day_cruise.sails || 0,
-                        engine: record.day_cruise.engine || 0,
-                        time: record.day_cruise.time || 0,
+                        sails: record.day_cruise ? record.day_cruise.sails || 0 : 0,
+                        engine: record.day_cruise ? record.day_cruise.engine || 0 : 0,
+                        time: record.day_cruise ? record.day_cruise.time || 0 : 0,
                     }));
 
                     setCruises(formattedCruises);
@@ -131,9 +136,11 @@ const Home = ({ navigation }: Props) => {
                 }
             }catch(err){
                 console.log(err);
+            }finally{
+                setLoading(false);
             }
-        }
-    }
+        };
+    };
 
     // Chart
     const currentDayIndex = new Date().getDay();
@@ -189,103 +196,117 @@ const Home = ({ navigation }: Props) => {
         getCruise();
         getUserAvatar();
     }, []);
+
+    useFocusEffect(
+        useCallback(() => {
+            getCruise();
+        }, [])
+    );
     
     return (
-        <SafeAreaView>
-            <View style={{position: 'relative', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end', paddingRight: 10, marginBottom: 20}}>
-                <View style={{position: 'absolute', left: 10, width: '81.5%', height: 60, borderWidth: 2, borderColor: '#555', borderRightWidth: 0, borderTopStartRadius: 15, borderBottomStartRadius: 15, padding: 10, justifyContent: 'center'}}>
-                    {loadingWeather ? (
-                        <Text style={{fontSize: 16, fontWeight: 600, color: '#808080'}}>Loading weather...</Text>
-                    ) : locationError ? (
-                        <Text>{locationError}</Text>
-                    ) : weatherData && weatherData.list ? (
-                        <View>
-                            <Text style={{ fontSize: 16, fontWeight: '600' }}>{weatherData.city?.name || 'Unknown location'}</Text>
-                            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                <Text style={{ fontSize: 16, fontWeight: '600', marginEnd: 10 }}>
-                                    {weatherData.list[0].main.temp !== undefined ? `${Math.round(weatherData.list[0].main.temp)}°C` : 'N/A'}
-                                </Text>
-                                <MaterialCommunityIcons name={getWeatherIcon(weatherData.list[0].weather[0]?.main || '', isDay)} size={24} color="black" style={{marginEnd: 5}}/>
-                                {weatherData.list.slice(0, 7).map((forecast: any, index: any) => (
-                                    <MaterialCommunityIcons
-                                        key={index}
-                                        name={getWeatherIcon(forecast.weather[0]?.main || '', isDay)}
-                                        size={24}
-                                        color="black"
-                                        style={{marginEnd: 5}}
-                                    />
-                                ))}
-                            </View>
+        <SafeAreaView style={{flex: 1}}>
+            { loading ? (
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <ActivityIndicator size="large" color="#084575" />
+                </View>
+            ) : (
+                <View style={{flex: 1}}>
+                    <View style={{position: 'relative', flexDirection: 'row', alignItems: 'center', justifyContent: 'flex-end', paddingRight: 10, marginBottom: 20}}>
+                        <View style={{position: 'absolute', left: 10, width: '81.5%', height: 60, borderWidth: 2, borderColor: '#555', borderRightWidth: 0, borderTopStartRadius: 15, borderBottomStartRadius: 15, padding: 10, justifyContent: 'center'}}>
+                            {loadingWeather ? (
+                                <Text style={{fontSize: 16, fontWeight: 600, color: '#808080'}}>Loading weather...</Text>
+                            ) : locationError ? (
+                                <Text>{locationError}</Text>
+                            ) : weatherData && weatherData.list ? (
+                                <View>
+                                    <Text style={{ fontSize: 16, fontWeight: '600' }}>{weatherData.city?.name || 'Unknown location'}</Text>
+                                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                                        <Text style={{ fontSize: 16, fontWeight: '600', marginEnd: 10 }}>
+                                            {weatherData.list[0].main.temp !== undefined ? `${Math.round(weatherData.list[0].main.temp)}°C` : 'N/A'}
+                                        </Text>
+                                        <MaterialCommunityIcons name={getWeatherIcon(weatherData.list[0].weather[0]?.main || '', isDay)} size={24} color="black" style={{marginEnd: 5}}/>
+                                        {weatherData.list.slice(0, 7).map((forecast: any, index: any) => (
+                                            <MaterialCommunityIcons
+                                                key={index}
+                                                name={getWeatherIcon(forecast.weather[0]?.main || '', isDay)}
+                                                size={24}
+                                                color="black"
+                                                style={{marginEnd: 5}}
+                                            />
+                                        ))}
+                                    </View>
+                                </View>
+                            ) : (
+                                <Text>Weather data unavailable</Text>
+                            )}
                         </View>
-                    ) : (
-                        <Text>Weather data unavailable</Text>
-                    )}
-                </View>
-                <TouchableOpacity>
-                    <Image 
-                        source={url ? { uri: url } : require('../assets/images/avatar.png')}
-                        style={{width: 75, height: 75, borderRadius: 50}}
-                    />
-                </TouchableOpacity>
-            </View>
-            <View style={{alignItems: 'center'}}>
-                <Text style={{fontSize: 18, fontWeight: 600, textTransform: 'uppercase', marginBottom: 10}}>Sailed miles this week</Text>
-                <BarChart 
-                   style={{ borderRadius: 16, justifyContent: 'center', alignItems: 'center', shadowColor: '#000', shadowOffset: { width: 0, height: 0 }, shadowOpacity: 0.25, shadowRadius: 3.84, elevation: 1 }}
-                   data={chartData}
-                   width={screenWidth - 20}
-                   height={180}
-                   yAxisLabel=''
-                   yAxisSuffix=''
-                   chartConfig={chartConfig}
-                   showBarTops={false}
-                   withHorizontalLabels={false}
-                   fromZero={true}
-                   showValuesOnTopOfBars={true}
-                />
-            </View>
-            <View style={{marginTop: 20, paddingHorizontal: 10}}>
-                <View style={{width: '100%', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginBottom: 5}}>
-                    <Text style={{fontSize: 18, fontWeight: 600, textTransform: 'uppercase'}}>Cruises</Text>
-                    <TouchableOpacity onPress={() => navigation.navigate('Cruise')}>
-                        <Text style={{fontSize: 16, fontWeight: 600, textTransform: 'uppercase', color: '#808080'}}>Pridať plabu</Text>
-                    </TouchableOpacity>
-                </View>
-                <ScrollView style={{borderTopWidth: 0.2, borderColor: 'rgba(0,0,0,0.2)', paddingTop: 5}}>
-                    {cruises.length > 0 ?
-                        cruises.map(cruise => (
-                            <TouchableOpacity style={{width: '100%', height: 125, borderWidth: 2, borderRadius: 16, padding: 10, marginBottom: 10}} key={cruise.id}>
-                                <View style={{flexDirection: 'row', flex: 1.5, gap: 10}}>
-                                    <Image 
-                                        source={require('../assets/images/imgage.png')}
-                                        style={{height: '100%', width: 50}}    
-                                    />
-                                    <View style={{display: 'flex'}}>
-                                        <Text style={{fontSize: 22, fontWeight: 600}}>{cruise.country}</Text>
-                                        <Text style={{fontSize: 12, paddingLeft: 1}}>{cruise.date}</Text>
-                                    </View>
-                                </View>
-                                <View style={{flexDirection: 'row', flex: 1, gap: 15, alignItems: 'flex-end'}}>
-                                    <View>
-                                        <Text style={{fontSize: 14, fontWeight: 600, textAlign: 'center'}}>Plachty</Text>
-                                        <Text style={{fontSize: 13, textAlign: 'center'}}>{cruise.sails} mi</Text>
-                                    </View>
-                                    <View>
-                                        <Text style={{fontSize: 14, fontWeight: 600, textAlign: 'center'}}>Motor</Text>
-                                        <Text style={{fontSize: 13, textAlign: 'center'}}>{cruise.engine} mi</Text>
-                                    </View>
-                                    <View>
-                                        <Text style={{fontSize: 14, fontWeight: 600, textAlign: 'center'}}>Čas</Text>
-                                        <Text style={{fontSize: 13, textAlign: 'center'}}>{cruise.time}</Text>
-                                    </View>
-                                </View>
+                        <TouchableOpacity>
+                            <Image 
+                                source={url ? { uri: url } : require('../assets/images/avatar.png')}
+                                style={{width: 75, height: 75, borderRadius: 50}}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{alignItems: 'center'}}>
+                        <Text style={{fontSize: 18, fontWeight: 600, textTransform: 'uppercase', marginBottom: 10}}>Sailed miles this week</Text>
+                        <BarChart 
+                           style={{ borderRadius: 16, justifyContent: 'center', alignItems: 'center', shadowColor: '#000', shadowOffset: { width: 0, height: 0 }, shadowOpacity: 0.25, shadowRadius: 3.84, elevation: 1 }}
+                           data={chartData}
+                           width={screenWidth - 20}
+                           height={180}
+                           yAxisLabel=''
+                           yAxisSuffix=''
+                           chartConfig={chartConfig}
+                           showBarTops={false}
+                           withHorizontalLabels={false}
+                           fromZero={true}
+                           showValuesOnTopOfBars={true}
+                        />
+                    </View>
+                    <View style={{flex: 1, marginTop: 20, paddingHorizontal: 10}}>
+                        <View style={{width: '100%', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginBottom: 5}}>
+                            <Text style={{fontSize: 18, fontWeight: 600, textTransform: 'uppercase'}}>Cruises</Text>
+                            <TouchableOpacity onPress={() => navigation.navigate('Cruise')}>
+                                <Text style={{fontSize: 16, fontWeight: 600, textTransform: 'uppercase', color: '#808080'}}>Pridať plabu</Text>
                             </TouchableOpacity>
-                        )) : (
-                            <Text style={{fontSize: 16, fontWeight: 600, textAlign: 'center'}}>No cruises found</Text>
-                        )
-                    }    
-                </ScrollView>
-            </View>
+                        </View>
+                        <ScrollView style={{borderTopWidth: 0.2, borderColor: 'rgba(0,0,0,0.2)', paddingTop: 5}}>
+                            {cruises.length > 0 ?
+                                cruises.map(cruise => (
+                                    <TouchableOpacity style={{width: '100%', height: 125, borderWidth: 2, borderRadius: 16, padding: 10, marginBottom: 10}} key={cruise.id}>
+                                        <View style={{flexDirection: 'row', flex: 1.5, gap: 10}}>
+                                            <Image 
+                                                source={require('../assets/images/imgage.png')}
+                                                style={{height: '100%', width: 50}}    
+                                            />
+                                            <View style={{display: 'flex'}}>
+                                                <Text style={{fontSize: 22, fontWeight: 600}}>{cruise.country}</Text>
+                                                <Text style={{fontSize: 12, paddingLeft: 1}}>{cruise.date}</Text>
+                                            </View>
+                                        </View>
+                                        <View style={{flexDirection: 'row', flex: 1, gap: 15, alignItems: 'flex-end'}}>
+                                            <View>
+                                                <Text style={{fontSize: 14, fontWeight: 600, textAlign: 'center'}}>Plachty</Text>
+                                                <Text style={{fontSize: 13, textAlign: 'center'}}>{cruise.sails} mi</Text>
+                                            </View>
+                                            <View>
+                                                <Text style={{fontSize: 14, fontWeight: 600, textAlign: 'center'}}>Motor</Text>
+                                                <Text style={{fontSize: 13, textAlign: 'center'}}>{cruise.engine} mi</Text>
+                                            </View>
+                                            <View>
+                                                <Text style={{fontSize: 14, fontWeight: 600, textAlign: 'center'}}>Čas</Text>
+                                                <Text style={{fontSize: 13, textAlign: 'center'}}>{cruise.time}</Text>
+                                            </View>
+                                        </View>
+                                    </TouchableOpacity>
+                                )) : (
+                                    <Text style={{fontSize: 16, fontWeight: 600, textAlign: 'center'}}>No cruises found</Text>
+                                )
+                            }    
+                        </ScrollView>
+                    </View>  
+                </View>
+            )}
         </SafeAreaView>
     );
 }
