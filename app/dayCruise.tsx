6b1e81b764ef7955d611663842b1d68e089955a5
Mathias <matejcikmathias@gmail.com>
2025-01-25 13:21:47 +0100

    Created initial day cruise screen

---
+++ app/dayCruise.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { SafeAreaView, View, Text, ScrollView, TouchableOpacity } from 'react-native';
+
+const DayCruise = ({ dayCruise, navigation }: any) => {
+
+    return (
+        <SafeAreaView>
+            <Text>Deň</Text>
+        </SafeAreaView>
+    );
+}
+
+export default DayCruise;
