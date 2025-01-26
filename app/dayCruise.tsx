f839a45a18354acc09a561f2aea02e497549f5b3
Mathias <matejcikmathias@gmail.com>
2025-01-26 13:43:45 +0100

    Created header with day name, date and ports

--- app/dayCruise.tsx
+++ app/dayCruise.tsx
@@ -1,11 +1,47 @@
-import React from 'react';
-import { SafeAreaView, View, Text, ScrollView, TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, View, Text, ScrollView, TouchableOpacity, ActivityIndicator, Image } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Pocketbase from 'pocketbase';
 
-const DayCruise = ({ dayCruise, navigation }: any) => {
+const DayCruise = ({ route, navigation }: any) => {
+    const { cruise, dayCruise } = route.params;
+
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+    }, []);
 
     return (
-        <SafeAreaView>
-            <Text>Deň</Text>
+        <SafeAreaView style={{ flex: 1 }}>
+            { loading ? (
+                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                    <ActivityIndicator size="large" color="#084575" />
+                </View>
+            ) : (
+                <View style={{ flex: 1 }}>
+                    <TouchableOpacity style={{flexDirection: 'row', alignItems: 'center', gap: 2, paddingInline: 10, marginBottom: 5}} onPress={() => navigation.navigate('Cruise', { cruise })}>
+                        <Ionicons name='arrow-back' size={18} color='#084575' />
+                        <Text style={{fontSize: 16, fontWeight: 500, color: '#084575'}}>Späť</Text>
+                    </TouchableOpacity>
+                    <View style={{flexDirection: 'row', paddingRight: 10, paddingBottom: 15, paddingTop: 5, alignItems: 'center', justifyContent: 'space-between', borderBottomWidth: 1, borderColor: 'rgba(0,0,0,0.2)'}}>
+                        <View style={{paddingInline: 10}}>
+                            <Text style={{width: '100%', fontSize: 26, fontWeight: 700, textTransform: 'uppercase'}}>{dayCruise.day}</Text>
+                            <Text style={{fontWeight: 500, color: '#808080'}}>{dayCruise.date}</Text>
+                        </View>
+                        <View>
+                            <View style={{flexDirection: 'row', alignItems: 'center', gap: 5}}>
+                                <Text style={{fontSize: 16, fontWeight: 500}}>Z prístavu:</Text>
+                                <Text style={{fontSize: 16}}>{dayCruise.from_port}</Text>
+                            </View>
+                            <View style={{flexDirection: 'row', alignItems: 'center', gap: 5}}>
+                                <Text style={{fontSize: 16, fontWeight: 500}}>Do prístavu:</Text>
+                                <Text style={{fontSize: 16}}>{dayCruise.to_port}</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+            )}
         </SafeAreaView>
     );
 }
