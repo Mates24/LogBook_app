c481d1149d2b03214473d841cd9f1027743afb4e
Mathias <matejcikmathias@gmail.com>
2025-02-22 13:17:11 +0100

    Finished modal with informations

--- app/hourRecord.tsx
+++ app/hourRecord.tsx
@@ -1,15 +1,47 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, View, Text, ScrollView, TouchableOpacity, ActivityIndicator, TextInput, StyleSheet, Alert } from 'react-native';
+import { SafeAreaView, View, Text, ScrollView, TouchableOpacity, ActivityIndicator, TextInput, StyleSheet, Alert, FlatList, Dimensions } from 'react-native';
+import { Table, Row, Rows } from 'react-native-table-component';
 import { Ionicons } from '@expo/vector-icons';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Pocketbase from 'pocketbase';
-import { get } from 'react-native/Libraries/TurboModule/TurboModuleRegistry';
 
 const HourRecord = ({ route, navigation }: any) => {
     const { cruise, dayCruise, hourRecord } = route.params;
-
+    
     const [loading, setLoading] = useState(true);
     const [updatedRecord, setUpdatedRecord] = useState<any[]>([]);
+    
+    // Show info
+    const [showInfo, setShowInfo] = useState(false);
+
+    // Pages for info
+    const [pageOne, setPageOne] = useState(true);
+    const [pageTwo, setPageTwo] = useState(false);
+
+    // Beaufortova stupnica
+    const data = [
+        ['0', 'pod 1', '-', 'bezvetrie'],
+        ['1', '1 - 3', '0,1', 'vánok'],
+        ['2', '4 - 6', '0,2', 'slabý vietor'],
+        ['3', '7 - 10', '0,6', 'mierny vietor'],
+        ['4', '11 - 16', '1', 'dosť čerstvý vietor'],
+        ['5', '17 - 21', '2', 'čerstvý vietor'],
+        ['6', '22 - 27', '3', 'silný vietor'],
+        ['7', '28 - 33', '4', 'prudký vietor'],
+        ['8', '34 - 40', '5,5', 'búrlivý vietor'],
+        ['9', '41 - 47', '7', 'víchrica'],
+        ['10', '48 - 55', '9', 'silná víchrica'],
+        ['11', '56 - 63', '11,5', 'mohutná víchria'],
+        ['12', '64 a viac', '14', 'orkán'],
+    ];
+    const tableHead = ['Stupeň', 'Rýchlosť vetra (uzly)', 'Výška vĺn (metre)', 'Slovné označenie'];
+    const screenWidth = Dimensions.get('window').width;
+
+    const handleInfo = () => {
+        setPageOne(true);
+        setPageTwo(false);
+        setShowInfo(!showInfo);
+    };
 
     // Inputs for navigation
     const [lat, setLat] = useState('');
@@ -161,11 +193,64 @@ const HourRecord = ({ route, navigation }: any) => {
                 </View>
             ) : (
                 <View style={{flex: 1}}>
+                    { showInfo && (
+                        <View style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', backgroundColor: '#151718', justifyContent: 'center', alignItems: 'center', zIndex: 100}}>
+                            <View style={{backgroundColor: '#fff', padding: 20, borderRadius: 10}}>
+                                <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 10}}>
+                                    <Text style={{fontSize: 20, fontWeight: 600, textTransform: 'uppercase'}}>Zápis</Text>
+                                    <TouchableOpacity onPress={handleInfo}>
+                                        <Ionicons name='close' size={30} color='#F62F2F' />
+                                    </TouchableOpacity>
+                                </View>
+                                { pageOne && (
+                                    <View style={{width: '100%'}}>
+                                        <Text style={styles.infoText}><Text style={styles.boldText}>1. LAT</Text> - Zemepisná šírka polohy</Text>
+                                        <Text style={styles.infoText}><Text style={styles.boldText}>2. LONG</Text> - Zemepisná dĺžka polohy</Text>
+                                        <Text style={styles.infoText}><Text style={styles.boldText}>3. KK</Text> - Kurz kompasový, vypisuje sa v stupňoch od 0° do 360°</Text>
+                                        <Text style={styles.infoText}><Text style={styles.boldText}>4. KD</Text> - Kurz dnový, vypisuje sa v stupňoch od 0° do 360° </Text>
+                                        <Text style={styles.infoText}><Text style={styles.boldText}>5. Deviácia</Text> - Odchýlka od kurzu</Text>
+                                        <Text style={styles.infoText}><Text style={styles.boldText}>6. Rýchlosť</Text> - Rýchlosť plavby voči dnu v uzloch</Text>
+                                        <Text style={{fontSize: 16, marginBottom: 5}}><Text style={styles.boldText}>7. Plachty</Text> - Uvádzajú sa skratky plachiet: {'\n'}         HP - hlavná plachta {'\n'}         K - kosatka {'\n'}         G - genua {'\n'}         S - spinaker</Text>
+                                        <Text style={styles.infoText}>    Refovanie sa uvádza podľa plochy plachiet v %</Text>
+                                        <Text style={styles.infoText}><Text style={styles.boldText}>8. Motor</Text> - Pri plavbe na motor sa uvádzajú otáčky motora</Text>
+                                        <Text style={styles.infoText}><Text style={styles.boldText}>9. Vietor</Text> - Smer vetra sa zapisuje skratkou, sila vetra podľa Beaufortovej stupnice napr. SW 5</Text>
+                                        <Text style={styles.infoText}><Text style={styles.boldText}>10. Oblačnosť</Text> - Stupeň oblačnosti sa zapisuje následovne: {'\n'}          0 - jasno (bez oblakov) {'\n'}          1 - oblaky pokrývajú 1/3 oblohy {'\n'}          2 - oblaky pokrývajú 1/2 oblohy {'\n'}          3 - oblaky pokrývajú 3/4 oblohy {'\n'}          4 - obloha úplne zatiahnutá {'\n'}          X - oblohu nie je vidno, hmla</Text>
+                                        <Text style={styles.infoText}><Text style={styles.boldText}>11. Teplota</Text> - Zapisuje sa v stupňoch Celzia</Text>
+                                        <Text style={styles.infoText}><Text style={styles.boldText}>12. Viditeľnosť</Text> - Zapiseuje sa v Nm</Text>
+                                    </View>
+                                )}
+                                { pageTwo && (
+                                    <View style={{width: screenWidth * 0.89, marginBottom: 10}}>
+                                        <Text style={{fontSize: 16, fontWeight: 500, marginBottom: 5}}>Beaufortova stupnica</Text>
+                                        <Table borderStyle={{borderWidth: 1, borderColor: '#C1C0B9'}}>
+                                          <Row data={tableHead} style={styles.head} textStyle={styles.text} />
+                                          {data.map((rowData, index) => (
+                                            <Row key={index} data={rowData} style={styles.row} textStyle={styles.text} />
+                                          ))}
+                                        </Table>
+                                  </View>
+                                )}
+                                <View style={{flexDirection: 'row', justifyContent: 'center', marginTop: 10}}>
+                                    <TouchableOpacity onPress={() => { setPageOne(true); setPageTwo(false); }}>
+                                        <Ionicons name={pageOne ? 'radio-button-on' : 'radio-button-off'} size={18} color='#808080' />
+                                    </TouchableOpacity>
+                                    <TouchableOpacity onPress={() => { setPageOne(false); setPageTwo(true); }}>
+                                        <Ionicons name={pageTwo ? 'radio-button-on' : 'radio-button-off'} size={18} color='#808080' />
+                                    </TouchableOpacity>
+                                </View>
+                            </View>
+                        </View>
+                    )}
                     <View style={{paddingBottom: 15}}>
-                        <TouchableOpacity style={{flexDirection: 'row', alignItems: 'center', gap: 2, paddingInline: 10, marginBottom: 5}} onPress={() => navigation.navigate('DayCruise', { cruise, dayCruise: { ...dayCruise, hour_record: updatedRecord } })}>
-                            <Ionicons name='arrow-back' size={18} color='#084575' />
-                            <Text style={{fontSize: 16, fontWeight: 500, color: '#084575'}}>Späť</Text>
-                        </TouchableOpacity>
+                        <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingInline: 10, marginBottom: 5}}>
+                            <TouchableOpacity style={{flexDirection: 'row', alignItems: 'center', gap: 2}} onPress={() => navigation.navigate('DayCruise', { cruise, dayCruise: { ...dayCruise, hour_record: updatedRecord } })}>
+                                <Ionicons name='arrow-back' size={18} color='#084575' />
+                                <Text style={{fontSize: 16, fontWeight: 500, color: '#084575'}}>Späť</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={handleInfo}>
+                                <Ionicons name='information-circle-outline' size={20} color='#084575' />
+                            </TouchableOpacity>   
+                        </View>
                         <View style={{flexDirection: 'row', paddingRight: 10, paddingBottom: 15, paddingTop: 5, alignItems: 'center', justifyContent: 'space-between', borderBottomWidth: 1, borderColor: 'rgba(0,0,0,0.2)'}}>
                             <View style={{paddingInline: 10}}>
                                 <Text style={{width: '100%', fontSize: 26, fontWeight: 700, textTransform: 'uppercase'}}>{dayCruise.day}</Text>
@@ -376,5 +461,26 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 10,
         borderColor: '#808080',
-    }
+    },
+
+    infoText: {
+        fontSize: 16,
+        marginBottom: 5
+    },
+
+    boldText: {
+        fontWeight: '600'
+    },
+
+    head: {
+        height: 40,
+        backgroundColor: '#dcdcdc',
+    },
+    row: {
+        height: 40,
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: 13,
+    },
 });
